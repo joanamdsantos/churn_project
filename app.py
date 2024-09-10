5892f6747fbc71d5e8ac18675d7a75c8e44be2e6
joanamdsantos <joanamlmsantos@gmail.com>
2024-09-10 13:00:13 +0100

    Fix: port number

--- app.py
+++ app.py
@@ -129,4 +129,4 @@ def download_file(filename):
 
 if __name__ == '__main__':
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-    app.run(debug=True, host='0.0.0.0', port=5050)
+    app.run(debug=True, host='0.0.0.0', port=5000)
